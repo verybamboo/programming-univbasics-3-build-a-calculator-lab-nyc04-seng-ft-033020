9faa31b4e4dfab9a699ef105bee16e359176dad7
George Chen <georgechen1338@gmail.com>
2020-03-17 23:33:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,21 +5,24 @@ end
 
 def subtraction(num1, num2)
   difference = num1 - num2
-  
+  puts difference
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  puts quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  puts product
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  puts remainder
 end
 
 def square_root(num)
-
+  root = num
 end
